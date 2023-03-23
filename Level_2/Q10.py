e3937e9426e16252a00b3ecc1c5cafaafd757a8e
jeanmeza <jc.meza@sis-net.it>
2023-03-23 16:46:40 +0100

    questions 9 and 10

---
+++ Level_2/Q10.py
@@ -0,0 +1,2 @@
+sequence = input().split(' ')
+print(" ".join(sorted(list(set(sequence)))))
---
+++ Level_2/Q9.py
@@ -0,0 +1,12 @@
+lines = []
+while True:
+    s = input()
+    if s:
+        lines.append(s.upper())
+    else:
+        break
+
+for line in lines:
+    print(line)
+
+
